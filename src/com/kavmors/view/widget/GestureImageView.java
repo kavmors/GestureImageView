cf47a0bdbe0ff665921643bb9ac7628a526f4c01
KavMors <kavmors@163.com>
2016-03-08 17:17:36 +0800

    Fix a bug

--- src/com/kavmors/view/widget/GestureImageView.java
+++ src/com/kavmors/view/widget/GestureImageView.java
@@ -113,7 +113,6 @@ public class GestureImageView extends ImageView {
 	
 	private void privateConstructor(Context context) {
 		mMatrix.set(getImageMatrix());
-		setBackgroundColor(Color.BLACK);
 		super.setScaleType(ScaleType.MATRIX);
 		super.setLongClickable(false);		//Mask View.LongClick
 		setClickable(true);
